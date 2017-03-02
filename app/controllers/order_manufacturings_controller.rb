868517481e663dba912d6bbbd3995b4a7dab36e5
rem <kremenchuk@bk.ru>
2017-03-02 12:30:50 +0200

    datatables

--- app/controllers/order_manufacturings_controller.rb
+++ app/controllers/order_manufacturings_controller.rb
@@ -2,13 +2,13 @@ class OrderManufacturingsController < ApplicationController
   layout false
 
   def index
+    # SELECT c.id FROM counterparties as c WHERE c.name like :search
     data_hash = {
         view_context: view_context,
         sort_column: %w[date number counterparty.name invoice],
         model: OrderManufacturing,
-        search_query: 'date like :search or number like :search or
-(order_manufacturings.counterparty_id = SELECT c.id FROM counterparties as c WHERE c.name like :search) or
-like :search or invoice like :search'.delete("\n")
+        search_query: 'date like :search or number like :search or counterparties.id IN :join_table_var or invoice like :search',
+        join_table: 'counterparties'
     }
 
     respond_to do |format|
--- app/datatables/datatable_class.rb
+++ app/datatables/datatable_class.rb
@@ -6,6 +6,10 @@ class DatatableClass
     @sort_column = data_hash[:sort_column]
     @model = data_hash[:model]
     @search_query = data_hash[:search_query]
+    if data_hash[:join_table].present?
+      @join_table = data_hash[:join_table]
+    end
+
   end
 
   def as_json(options = {})
@@ -50,7 +54,14 @@ class DatatableClass
     model_data_var = @model.order("#{sort_column} #{sort_direction}")
     model_data_var = model_data_var.page(page).per_page(per_page)
     if params[:sSearch].present?
-      model_data_var = model_data_var.where(@search_query, search: "%#{params[:sSearch]}%")
+      if @join_table.present?
+        model_data_var = model_data_var.join(@join_table)
+                             .where(@search_query, join_table_var: "SELECT #{@join_table}.id FROM #{@join_table}.name like :search", search: "%#{params[:sSearch]}%")
+      else
+        model_data_var = model_data_var.where(@search_query, search: "%#{params[:sSearch]}%")
+      end
+
+
     end
     model_data_var
   end
