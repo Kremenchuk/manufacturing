a155133ce1cc56ba9e7555f5561317b3966d7c95
kremenchuk <kremartem@gmail.com>
2021-07-14 16:35:17 +0300

    many changes

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,7 +1,7 @@
 class Material < ApplicationRecord
   enum unit: ['шт', 'м/п', 'кг']
 
-  default_scope { order(created_at: :qty) }
+  default_scope { order(qty: :desc) }
 
   has_many :order_manufacturings_details, as: :order_manufacturings_detailable
 
