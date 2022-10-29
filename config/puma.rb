4f4886c7d7c4d5f8e82b510bdbed62c9e72f5fce
rem <kremartem@gmail.com>
2022-10-29 22:36:02 +0300

    2

--- config/puma.rb
+++ config/puma.rb
@@ -2,36 +2,36 @@
 rails_env = ENV['RAILS_ENV'] || "development"
 environment rails_env
 
-if rails_env == "production"
-  p "===IN PRODUCTION MODE==="
-  # Change to match your CPU core count
-  workers 2
-
-  # Min and Max threads per worker
-  threads 1, 6
-
-  app_dir = File.expand_path("../..", __FILE__)
-  shared_dir = "#{app_dir}/shared"
-
-
-
-  # Set up socket location
-  bind "unix://#{shared_dir}/sockets/puma.sock"
-
-  # Logging
-  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
-
-  # Set master PID and state locations
-  pidfile "#{shared_dir}/pids/puma.pid"
-  state_path "#{shared_dir}/pids/puma.state"
-  activate_control_app
-
-  on_worker_boot do
-    require "active_record"
-    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-  end
-else
+# if rails_env == "production"
+#   p "===IN PRODUCTION MODE==="
+#   # Change to match your CPU core count
+#   workers 2
+#
+#   # Min and Max threads per worker
+#   threads 1, 6
+#
+#   app_dir = File.expand_path("../..", __FILE__)
+#   shared_dir = "#{app_dir}/shared"
+#
+#
+#
+#   # Set up socket location
+#   bind "unix://#{shared_dir}/sockets/puma.sock"
+#
+#   # Logging
+#   stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+#
+#   # Set master PID and state locations
+#   pidfile "#{shared_dir}/pids/puma.pid"
+#   state_path "#{shared_dir}/pids/puma.state"
+#   activate_control_app
+#
+#   on_worker_boot do
+#     require "active_record"
+#     ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+#     ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+#   end
+# else
   p "===IN DEVELOPMENT MODE==="
   # Puma can serve each request in a thread from an internal thread pool.
   # The `threads` method setting takes two numbers a minimum and maximum.
@@ -80,5 +80,5 @@ else
 
   # Allow puma to be restarted by `rails restart` command.
   plugin :tmp_restart
-end
+# end
 
